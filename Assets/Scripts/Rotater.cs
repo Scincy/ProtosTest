90161e23b919fbc25cbe8db4ed60f70725f3e584
Junyoung Yoon <uji3210@gmail.com>
2020-08-21 22:35:26 +0900

    회전 타일 추가
    호ㅣ전하는 타일을 추가했습니다.

---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum RotState { Idle, Stopped, RightRotation, LeftRotation }
+public class Rotater : MonoBehaviour
+{
+
+    public float rotationSpeed;
+    public RotState currentRotStste = RotState.Idle;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentRotStste)
+        {
+            case RotState.Idle:
+                break;
+            case RotState.Stopped:
+                break;
+            case RotState.RightRotation:
+                transform.Rotate(new Vector3(0, 0, -rotationSpeed * Time.deltaTime));
+                break;
+            case RotState.LeftRotation:
+                transform.Rotate(new Vector3(0, 0, rotationSpeed * Time.deltaTime));
+                break;
+            default:
+                break;
+        }
+        
+    }
+}
