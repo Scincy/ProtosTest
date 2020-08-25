cd86f3b832021940db79ea6b0eb1b2558779e4e9
OmegaThree <devomegathree@gmail.com>
2020-08-25 15:04:54 +0900

    adsf
    asdf

---
+++ Assets/Scripts/AllClear.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllClear : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject != null)
+        {
+            Debug.Log(collision.gameObject);
+        }
+
+        if (collision.tag == "Player")
+        {
+            enabled = false;
+            GameObject.FindGameObjectWithTag("DialogManager").GetComponent<DialogManager>().ShowLobbyDialog("게임 클리어!");
+        }
+    }
+}
---
+++ Assets/Scripts/Clear.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clear : MonoBehaviour
+{
+    public Transform newRegenPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject != null)
+        {
+            Debug.Log(collision.gameObject);
+        }
+
+        if (collision.tag == "Player")
+        {
+            enabled = false;
+            collision.GetComponent<CharactorContorller>().ChangeRegenPoint(newRegenPoint.position);
+            GameObject.FindGameObjectWithTag("DialogManager").GetComponent<DialogManager>().ShowRegenDialog("스테이지 클리어!");
+        }
+    }
+}
